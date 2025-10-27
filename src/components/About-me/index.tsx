fab4723d90de036df303b7b7e352aa843a31c70b
Alison <alisonbetini27@gmail.com>
2025-10-27 15:54:54 -0300

    new fallowers 743

--- src/components/About-me/index.tsx
+++ src/components/About-me/index.tsx
@@ -56,7 +56,7 @@ export function AboutMe({ id }: AboutMeProps) {
         </p>
         <div className="separator-card">
           <div className="card">
-            <strong>702</strong>
+            <strong>743</strong>
             <span>Seguidores</span>
           </div>
           <div className="card">
